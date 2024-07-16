00043fc92e8a114eae54e2f80c90406cf5574bf2
vishva <hirparajyotsna@gmail.com>
2024-07-16 15:20:29 +0530

    15:20

---
+++ 7.react-js/React-Project/Random-colour/Rendom-colour/src/App.jsx
@@ -0,0 +1,12 @@
+import Randomcolor from "./Component/Randomcolor";
+
+
+function App() {
+  return (
+    <div>
+      <Randomcolor />
+    </div>
+  );
+}
+
+export default App;
---
+++ 7.react-js/React-Project/Random-colour/Rendom-colour/src/Component/Randomcolor.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import React from 'react'
+
+
+const Randomcolor = () => {
+    const [color, setColor] = useState(`#000`)
+
+    const getRgb = () => Math.floor(Math.random() * 256)
+
+    const rgbToHex = (r, g, b) => "# " + [r, g, b].map((x) => {
+        const hex = x.toString(16)
+        return hex.length === 1 ? "0" + hex : hex;
+    }).join("")
+
+    const handleGenerate = () => {
+        const color = {
+            r: getRgb(),
+            g: getRgb(),
+            b: getRgb(),
+        }
+        setColor(rgbToHex(color.r, color.g, color.b))
+    }
+
+    return (
+        <div style={{ backgroundColor: color }} className="container">
+            <button style={{ color: color }} onClick={handleGenerate}>
+                {color}
+            </button>
+        </div>
+    )
+}
+
+export default Randomcolor
